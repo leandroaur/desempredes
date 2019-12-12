895e5a1fd38fda688b4c920c6bcd1ff0ab15b71e
Leonardo Shibata <9448016+leonardoshibata@users.noreply.github.com>
2019-12-12 19:07:20 -0300

    Comentários e gráfico com a distribuição das observações

--- min_med_max.R
+++ min_med_max.R
@@ -1,15 +1,23 @@
 library(tidyverse)
 
+# carrega os dados
 nperf4g <- read.csv("data/nperfres4g.csv")
 
-nperf4g %>% as_tibble() %>%
-  select(SPEED_DOWNLOAD_AVG, NET_NAME, Period) %>%
-  group_by(NET_NAME, Period) %>%
+
+nperf4g %>%
+  select(SPEED_DOWNLOAD_AVG, NET_NAME, Period) %>% # seleciona as variáveis de interesse
+  group_by(NET_NAME, Period) %>%  # informa por quais variáveis agrupar para o comando seguinte
   summarise(
-    minimo = min(SPEED_DOWNLOAD_AVG),
-    media = mean(SPEED_DOWNLOAD_AVG, na.rm = TRUE),
-    maximo = max(SPEED_DOWNLOAD_AVG),
-   desvio = sd(SPEED_DOWNLOAD_AVG, na.rm = TRUE),
-)
-  
+    minimo = min(SPEED_DOWNLOAD_AVG), # o valor mínimo das observações
+    media = mean(SPEED_DOWNLOAD_AVG, na.rm = TRUE), # o valor médio das observações
+    maximo = max(SPEED_DOWNLOAD_AVG), # o valor máximo das observações
+   desvio = sd(SPEED_DOWNLOAD_AVG, na.rm = TRUE)) # o desvio padrão
+
+
+nperf4g %>%
+  select(SPEED_DOWNLOAD_AVG, NET_NAME, Period) %>%
+  ggplot(mapping = aes(x = Period, y = SPEED_DOWNLOAD_AVG)) + # gráfico com o Period no eixo x, e velocidade no y
+  geom_point( # plota pontos para cada observação
+    aes(color = NET_NAME), # cada operadora com uma cor diferente
+    position = "jitter") # espalha os pontos para não serem plotados um em cima do outro
 
